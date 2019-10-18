674d3758f582d9292612b2db3a143bb28f1e2fa3
Munanga <jackslaighter@gmail.com>
2019-10-18 12:07:00 -0500

    Finished adding all genres

--- NibblesBookProject/settings.py
+++ NibblesBookProject/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6=%rr!pgbg0ew4)h&h3&(3%u)6yf!#ou5571-w0_kbq%!$a9n5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["Munanga.pythonanywhere.com"]
+ALLOWED_HOSTS = ["Munanga.pythonanywhere.com", "127.0.0.1"]
 
 
 # Application definition
